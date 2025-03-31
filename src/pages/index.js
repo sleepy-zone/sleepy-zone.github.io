dfb648f3de4f512d86d5569210c99e60643276cc
sleepy-zone <micro_sobby@outlook.com>
2025-03-31 22:02:13 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import React from 'react';
 import Profile from './home/Profile';
-import About from './home/About';
 import Projects from './home/Projects';
 
 export default function Home() {
@@ -13,7 +12,6 @@ export default function Home() {
       description="sleepy zone blog, projects ...">
       <div className="app">
         <Profile />
-        <About />
         <Projects />
       </div>
     </Layout>
