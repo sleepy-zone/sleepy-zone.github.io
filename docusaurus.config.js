73f6a20b683d483a2bd6975df626e3ab56794d39
sleepy-zone <micro_sobby@outlook.com>
2025-04-08 18:34:53 +0800

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'sleepy-zone', // Usually your GitHub org/user name.
   projectName: 'blog', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
